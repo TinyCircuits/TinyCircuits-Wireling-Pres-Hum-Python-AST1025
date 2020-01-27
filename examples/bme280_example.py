f9482b5bab054ecb0888078307a100ff6c250afa
Rena <info@tinycircuits.com>
2020-01-27 17:22:34 -0500

    examples

---
+++ examples/bme280_example.py
@@ -0,0 +1,33 @@
+# Example prints the temperature, pressure, humidity, and altitude of the BME280
+# sensor
+# Library by: Adafruit
+# Example modified by: Laverena Wienclaw for TinyCircuits
+
+import time
+import board
+import busio
+import adafruit_bme280
+import tinycircuits_wireling
+
+# Enable power to pi hat and wirelings
+wireling = tinycircuits_wireling.Wireling()
+wireling.selectPort(0) # Select port (0-3) labeled on the Pi Hat
+
+# Create library object using our Bus I2C port
+i2c = busio.I2C(board.SCL, board.SDA)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+# OR create library object using our Bus SPI port
+#spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+#bme_cs = digitalio.DigitalInOut(board.D10)
+#bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
+
+# change this to match the location's pressure (hPa) at sea level
+bme280.sea_level_pressure = 1013.25
+
+while True:
+    print("\nTemperature: %0.1f C" % bme280.temperature)
+    print("Humidity: %0.1f %%" % bme280.humidity)
+    print("Pressure: %0.1f hPa" % bme280.pressure)
+    print("Altitude = %0.2f meters" % bme280.altitude)
+    time.sleep(2)
